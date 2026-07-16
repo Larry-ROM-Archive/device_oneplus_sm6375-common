d3bb8a250f10001987beb28ab7506e47db6894f6
Viaan Thakur <viaan4542@gmail.com>
2026-07-16 21:05:22 +0530

    sm6375-common: Add GameBar

--- sm6375.mk
+++ sm6375.mk
@@ -10,6 +10,8 @@ $(call inherit-product, hardware/qcom-caf/common/common.mk)
 # Dolby Atmos
 $(call inherit-product, hardware/dolby/dolby.mk)
 
+# GameBar Performance Overlay
+$(call inherit-product, packages/apps/GameBar/gamebar.mk)
 # A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
